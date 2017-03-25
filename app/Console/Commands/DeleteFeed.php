6f68cce190956385a03396c7c44b173e5c45a44c
Frederick Ankamah <frederickankamah988@gmail.com>
2017-03-25 18:13:48 +0000

    more popular products redirect

---
+++ app/Console/Commands/DeleteFeed.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\StreamFeed;
+use App\User;
+use Illuminate\Console\Command;
+
+class DeleteFeed extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'delete:feed {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete User Stream Feed';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $user_id = User::whereName($this->argument('name'))->first()->id;
+        $stream = new StreamFeed($user_id);
+        $stream->deleteFeed();
+    }
+}
