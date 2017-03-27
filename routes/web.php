1bef1240debcbf49131459701139061b24fdf89b
Frederick Ankamah <frederickankamah988@gmail.com>
2017-03-27 23:18:06 +0000

    checkout and cart restructuring

--- routes/web.php
+++ routes/web.php
@@ -62,15 +62,15 @@ Route::post('/watch-shop/{product_id}/{store_id}/{user_id}','HomeController@post
 Route::post('/register-user','HomeController@postRegisterUser');
 
 Route::group(['domain' => '{slug}.shopaholicks.com'], function () {
-    if (\Illuminate\Support\Facades\Auth::check()){
+//    if (\Illuminate\Support\Facades\Auth::check()){
         Route::get("shop", function ($slug) {
             $store = \App\Store::whereSlug($slug)->first();
 
             return redirect("stores/$slug/$store->user_id");
         });
-    }else {
+//    }else {
 
-    }
+//    }
 });
 
 Route::group(['prefix' => 'stores'], function () {
