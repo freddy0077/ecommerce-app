9c08bfdde9b91b1bd32ccef5e6efe4c1470dde7a
Frederick Ankamah <frederickankamah988@gmail.com>
2017-04-04 09:59:15 +0000

    Added

--- resources/views/store/layouts/store_layout.blade.php
+++ resources/views/store/layouts/store_layout.blade.php
@@ -170,7 +170,7 @@
                     <!-- Secondary menu -->
                     <div class="col-md-2 col-sm-5 col-xs-12 shopping_cart pull-right" id="shopping-cart">
                         <!--cart-->
-                        <div id="cart" class=" btn-group btn-shopping-cart">
+                        <div id="cart" class=" btn-group btn-shopping-cart hidden-lg hidden-md">
                             <a data-loading-text="Loading..." class="top_cart dropdown-toggle" data-toggle="dropdown">
                                 <div class="shopcart">
                                     <span class="handle pull-left"></span>
@@ -258,6 +258,95 @@
                             </ul>
                         </div>
                         <!--//cart-->
+
+                        <div id="cart" class=" btn-group btn-shopping-cart hidden-sm hidden-xs">
+                            <a data-loading-text="Loading..." class="top_cart dropdown-toggle" data-toggle="dropdown">
+                                <div class="shopcart">
+                                    <span class="handle pull-left"></span>
+                                    <span class="title">My cart</span>
+                                    <p class="text-shopping-cart cart-total-full">{{\Gloudemans\Shoppingcart\Facades\Cart::count()}} item(s) - GHS {{\Gloudemans\Shoppingcart\Facades\Cart::subtotal()}} </p>
+                                </div>
+                            </a>
+
+                            <ul class="tab-content content dropdown-menu pull-right shoppingcart-box" role="menu">
+
+                                <li>
+                                    <table class="table table-striped">
+                                        <tbody>
+
+                                        @foreach(\Gloudemans\Shoppingcart\Facades\Cart::content() as $content)
+
+                                            <tr>
+                                                <td class="text-center" style="width:70px">
+                                                    <a href="#">
+                                                        <img src="https://placehold.it/30x30" style="width:70px" alt="" title="" class="preview">
+                                                    </a>
+                                                </td>
+                                                <td class="text-left"> <a class="cart_product_name" href="#">{{$content->name}}</a> </td>
+                                                <td class="text-center"> x{{$content->qty}} </td>
+                                                <td class="text-center"> GHS {{$content->price}} </td>
+                                                <td class="text-right">
+                                                    <a href="#" class="fa fa-edit"></a>
+                                                </td>
+                                                <td class="text-right">
+                                                    <a onclick="cart.remove('{{$content->rowId}}','{{$user_id}}');" class="fa fa-times fa-delete"></a>
+                                                </td>
+                                            </tr>
+
+                                        @endforeach
+
+                                        </tbody>
+                                    </table>
+                                </li>
+                                <li>
+                                    <div>
+                                        <table class="table table-bordered">
+                                            <tbody>
+                                            <tr>
+                                                <td class="text-left"><strong>Sub-Total</strong>
+                                                </td>
+                                                <td class="text-right">{{\Gloudemans\Shoppingcart\Facades\Cart::subtotal()}}</td>
+                                            </tr>
+
+                                            {{--<tr>--}}
+                                            {{--<td class="text-left"><strong>VAT (20%)</strong>--}}
+                                            {{--</td>--}}
+                                            {{--<td class="text-right">$200.00</td>--}}
+                                            {{--</tr>--}}
+                                            <tr>
+                                                <td class="text-left"><strong>Total</strong>
+                                                </td>
+                                                <td class="text-right">{{\Gloudemans\Shoppingcart\Facades\Cart::subtotal()}}</td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <p class="text-right">
+                                            <a class="btn view-cart" href="{{url('/store/checkout',$user_id)}}">
+                                                <i class="fa fa-shopping-cart"></i>View Cart
+                                            </a>&nbsp;&nbsp;&nbsp;
+                                            <a class="btn  checkout-cart" id="check-out-redirect" data-checkout-id="{{$user_id}}"
+                                               href="{{url('store/checkout',$user_id)}}">
+                                                <i class="fa fa-share"></i>
+                                                Checkout
+                                            </a>
+                                        </p>
+
+                                        {{--<p class="text-right hidden-md hidden-lg">--}}
+                                        {{--<a href="{{url('')}}">View Checkout <i class="fa fa-shopping-cart"></i></a>--}}
+                                        {{--<a class="btn view-cart" href="{{url('/store/checkout',$user_id)}}">--}}
+                                        {{--<i class="fa fa-shopping-cart"></i>--}}
+                                        {{--View Cart--}}
+                                        {{--</a>&nbsp;&nbsp;&nbsp;--}}
+                                        {{--<a class="btn  checkout-cart" href="{{url('store/checkout',$user_id)}}">--}}
+                                        {{--<i class="fa fa-share"></i>--}}
+                                        {{--Checkout--}}
+                                        {{--</a>--}}
+                                        {{--</p>--}}
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+
                     </div>
 
                 </div>
