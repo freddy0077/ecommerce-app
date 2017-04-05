abdd43053563770bef002cf4eddc497b6299ae6c
Frederick Ankamah <frederickankamah988@gmail.com>
2017-04-05 18:02:10 +0000

    Deploy

---
+++ app/Events/FollowersEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Support\Facades\Event;
+
+class FollowersEvent extends Event implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets
+//        SerializesModels
+        ;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $chatMessage;
+    public $user;
+
+    public function __construct($chatMessage,$user)
+    {
+        $this->chatMessage = $chatMessage;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+//        return new PrivateChannel('channel-name');
+        return ['follow-channel'];
+
+    }
+}
---
+++ database/migrations/2017_04_05_111500_create_feeds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feeds', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('action');
+            $table->string('other')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feeds');
+    }
+}
