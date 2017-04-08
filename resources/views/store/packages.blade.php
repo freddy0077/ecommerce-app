83506e6e8bd88b8e414b9930779383d9a8d21ca1
Frederick Ankamah <frederickankamah988@gmail.com>
2017-04-08 09:17:42 +0000

    restructured login and sign up page

--- resources/views/store/packages.blade.php
+++ resources/views/store/packages.blade.php
@@ -147,6 +147,8 @@
                                             </div>
                                         </div>
                                         @endforeach
+                                        <br>
+                                        <br>
                                     </div>
                                 </div>
                             </div>
