065fd93ec0827105a762ec55dc86fe6dc387bfc6
Frederick Ankamah <frederickankamah988@gmail.com>
2017-04-18 12:02:50 +0000

    Added AWS

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -187,9 +187,10 @@
                             <div class="product-layout col-md-4 col-sm-6 col-xs-12 ">
                                 <div class="product-item-container">
                                     <div class="left-block">
-                                        <div class="product-image-container lazy second_img ">
-                                            <img data-src='{{asset("images/products/$product->image")}}' src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"  alt=" 30&quot;" class="img-responsive" />
-                                            <img data-src='{{asset("images/products/$product->image")}}' src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"  alt=" 30&quot;" class="img_0 img-responsive" />
+                                        <div class="product-image-container lazy  ">
+                                            <img data-src='{{asset("images/products/$product->image")}}'
+                                                 src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"  alt="" width="150" height="150 class="img-responsive" />
+{{--                                            <img data-src='{{asset("images/products/$product->image")}}' src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"  alt=" 30&quot;" class="img_0 img-responsive" />--}}
                                         </div>
                                         <!--Sale Label-->
                                         @if($product->sale)
